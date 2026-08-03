c4db272ec45228b4e117744b111674d8854a38fe
marcelobelinatofilho-sketch <marcelo.belinato.filho@escola.pr.gov.br>
2026-08-03 11:54:24 -0300

    s

---
+++ style.cc
@@ -0,0 +1,52 @@
+<head>
+ <meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Maritacas</title>
+
+ <style>
+ <header>
+  <h1>Meu blog passaros<h1/>
+  <h1>Conhecendo as maritacas e os papagaios</h1>
+  </head>     
+
+    <main>
+    <p> Boas-vindas ao meu blog, aqui irei falar sobre a vida das maritacas e papagaios e algumas curiosidades sobre eles</p>
+ <h1>Meu primeiro post</h1>
+                    <p> Por: Marcelo belinato filho</p>
+                    </main>
+
+                    
+                    header {
+                        background-color: #48ac0e
+                        color: #FFFFFF;
+                        text-align: center;
+                        max-width: 800px;
+                        margin: 0 auto;
+                        padding: 16px;
+                        
+                    }
+                    main{
+                        background-color: #48ac0e;
+                        color: #FFFFFF;
+                        text-align: center;
+                        max-width: 800px;
+                        margin: 0 auto;
+                        padding: 16px;
+                        
+                    }
+                    article{
+                        display: flex;
+                    }
+
+                    img{
+                        width: 80px;
+                        height: 80px;
+                        
+                    }
+
+                    .artigo-autor{
+                    font-weight: bold;
+                    }
+
+                    </style>
+                    </head>
